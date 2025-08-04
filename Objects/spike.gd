871355257ea9754762707c5b0b692394fe4d204d
Novaliyn <189510880+Novaliyn@users.noreply.github.com>
2025-08-04 14:02:50 -0700

    thing

--- Objects/spike.gd
+++ Objects/spike.gd
@@ -1,8 +1,9 @@
 extends Area2D
+@export var damage : int = 1
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Character:
-		body.lives -= 1
+		body.lives -= damage
 		print(body.lives)
 		if body.lives == 0:
 			print("you have failed. the mothership lives")
