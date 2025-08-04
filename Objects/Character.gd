f27d6979cf0e6bb8a47bcf343ee715cf96259b48
Novaliyn <189510880+Novaliyn@users.noreply.github.com>
2025-08-04 15:40:13 -0700

    added double jumping, and silver value changer

--- Objects/Character.gd
+++ Objects/Character.gd
@@ -5,13 +5,20 @@ var SPEED = 600.0
 var JUMP_VELOCITY = -500.0
 var score = 0
 var lives = 3
+var Jump = false
 
 func _physics_process(delta: float) -> void:
+	if not is_on_floor() and not is_on_wall() and Input.is_action_just_pressed("Spacebar") and Jump == false:
+		velocity.y = JUMP_VELOCITY
+		Jump = true
+	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 		
+	if is_on_floor():
+		Jump = false
 
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor() or Input.is_action_just_pressed("ui_accept") and is_on_wall():
+	if Input.is_action_just_pressed("Spacebar") and is_on_floor() or Input.is_action_just_pressed("Spacebar") and is_on_wall():
 		velocity.y = JUMP_VELOCITY
 
 	var direction := Input.get_axis("KEYA", "KEYD")
--- Objects/silver.gd
+++ Objects/silver.gd
@@ -1,7 +1,8 @@
 extends Area2D
+@export var value : int = 1
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Character:
-		body.score += 1
+		body.score += value
 		self.queue_free()
 		print(body.score)
