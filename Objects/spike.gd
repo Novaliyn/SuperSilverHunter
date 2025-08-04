8046b1079ccbbfedd693457374a5dbe65e69293a
Novaliyn <189510880+Novaliyn@users.noreply.github.com>
2025-08-04 14:44:38 -0700

    tilemap texture

--- Objects/spike.gd
+++ Objects/spike.gd
@@ -5,6 +5,6 @@ func _on_body_entered(body: Node2D) -> void:
 	if body is Character:
 		body.lives -= damage
 		print(body.lives)
-		if body.lives == 0:
+		if body.lives <= 0:
 			print("you have failed. the mothership lives")
 			get_tree().reload_current_scene()
