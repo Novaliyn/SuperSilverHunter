ca2bd44a53970fe8b56ef7ab7a9c6821552f0e53
Novaliyn <189510880+Novaliyn@users.noreply.github.com>
2025-08-04 18:28:04 -0700

    quicksave

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,1 +1,26 @@
 extends Node
+
+
+
+
+
+
+
+
+
+
+
+func unloadLevel():
+	if (is_instance_valid(levelInstance)):
+		levelInstance.queue_free();
+	levelInstance = null;
+   
+ func loadNewLevel(levelnumber):
+	unloadLevel();
+	var levelPath : String
+	if levelnumber is int:
+		levelPath = "res://Levels/Level%s.tscn" % str(levelnumber);
+		var levelResource : PackedScene = load(levelPath);
+		if levelResource:
+			levelInstance = levelResource.instantiate();
+			Gamelevel.add_child(levelInstance);
