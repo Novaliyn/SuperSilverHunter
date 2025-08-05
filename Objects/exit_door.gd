41a356c93dd014cc7fa64aba86f5e0705cbdbe03
Novaliyn <189510880+Novaliyn@users.noreply.github.com>
2025-08-04 17:06:08 -0700

    level templete, starting on level change script

--- Objects/exit_door.gd
+++ Objects/exit_door.gd
@@ -3,4 +3,5 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Character:
-		pass
+		var currentlevel = get_tree().current_scene.scene_file_path
+		var nextlevel = currentlevel.to_int() + 1
