fd1d491d5b149a5b2c58dabdb3cb929eae641a00
Nova <189510880+Novaliyn@users.noreply.github.com>
2025-08-08 15:05:06 -0700

    test

---
+++ Scripts/healthpack.gd
@@ -0,0 +1,8 @@
+extends Area2D
+@export var HealthPoints: int = 1
+
+func _on_body_entered(body: Node2D) -> void :
+    if body is Character and not Global.lives == 5:
+        Global.lives += 1
+        self.queue_free()
+    Signaler.UpdateScore.emit()
---
+++ Scripts/silver.gd
@@ -0,0 +1,8 @@
+extends Area2D
+@export var value: int = 1
+
+func _on_body_entered(body: Node2D) -> void :
+    if body is Character:
+        Global.score += value
+        self.queue_free()
+        Signaler.UpdateScore.emit()
