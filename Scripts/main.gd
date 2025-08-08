b169da47f0b84eb225f6bcaceffbfc3a0771b077
Jadenlong <jadenlong376@gmail.com>
2025-08-08 12:45:09 -0700

    UI fix

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -3,8 +3,9 @@ extends Node
 
 @onready var hud: Control = $HUD
 @onready var gamelevel: Node2D = $Gamelevel
-@onready var lives: Label = $HUD/HBoxContainer/Lives
-@onready var silver: Label = $HUD/HBoxContainer2/Silver
+@onready var lives: Label = $HUD/CanvasLayer/HBoxContainer2/Lives
+@onready var silver: Label = $HUD/CanvasLayer/HBoxContainer/Silver
+
 var levelInstance : Node2D
 
 
