f799a8ad6033d41ef647df2e1c4881fc899838b4
Nova <189510880+Novaliyn@users.noreply.github.com>
2025-08-08 17:13:38 -0700

    PowerCore

--- Scripts/level_3.gd
+++ Scripts/level_3.gd
@@ -3,5 +3,5 @@ extends Node2D
 @onready var animation_player: AnimationPlayer = $"Moving platforms/AnimationPlayer"
 
 
-func _ready() -> void :
-    animation_player.play("Forward")
+func _ready():
+	animation_player.play("Forward")
